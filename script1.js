26651f10699baa9ff094fc8b7e7f11f0ee682757
Karthikeyan Ramesh <87492274+Karthik9443@users.noreply.github.com>
2023-06-07 15:04:18 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -14,7 +14,16 @@ $(document).ready(function() {
 // Request permission to play the audio
 navigator.mediaDevices.getUserMedia({audio: true}).then(function(stream) {
 
+  // Create an audio element
+  var audio = document.createElement('audio');
 
+  // Set the audio source
+  audio.src = 'Joel_Adams_-_Please_Dont_Go_Lyric_(getmp3.pro).mp3';
+playButton.style.fontSize = "30px" ;
+
+  // Play the audio
+  audio.play();
+});
 
     function Vector2(_x, _y) {
         this.x = _x, this.y = _y;
